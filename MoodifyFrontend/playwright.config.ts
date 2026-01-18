8cfb652ce5e9c4d0733e1268a2ebcced5157766d
w <finwou@gmail.com>
2026-01-18 16:48:37 +0100

    set test to background

--- MoodifyFrontend/playwright.config.ts
+++ MoodifyFrontend/playwright.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests", // Playwright zoekt hier je tests
-  timeout: 60_000, // max 60 seconden per test
+  testDir: "./tests",
+  timeout: 60_000,
   retries: 1,
   use: {
-    headless: false, // false = je ziet de browser, true = achtergrond
+    headless: true, // false = browser, true = background
     baseURL: "https://4.251.168.14.nip.io",
     ignoreHTTPSErrors: true,
     viewport: { width: 1280, height: 720 },
